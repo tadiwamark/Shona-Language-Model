3008fbc06a242abcee83244e4e9979349a46983a
tadiwamark <64325379+tadiwamark@users.noreply.github.com>
2023-09-20 08:41:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from tensorflow.keras.models import load_model
 import streamlit as st
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
+import numpy as np
 
 # Initialize the tokenizer
 tokenizer = Tokenizer()
@@ -22,7 +23,9 @@ def predict_next_words(model, text, num_to_predict):
     for _ in range(num_to_predict):
         token_list = tokenizer.texts_to_sequences([text])[0]
         token_list = pad_sequences([token_list], maxlen=5, padding='pre')
-        predicted = model.predict_classes(token_list, verbose=0)
+        probabilities = model.predict(token_list, verbose=0)
+        predicted = np.argmax(probabilities, axis=-1)
+
         output_word = ""
         for word, index in tokenizer.word_index.items():
             if index == predicted:
