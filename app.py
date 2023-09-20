adf098c2c84d84fc62123302c97ac7275a343cc7
tadiwamark <64325379+tadiwamark@users.noreply.github.com>
2023-09-20 07:03:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 12:04:22 2023
+
+@author: tadiw
+"""
+
+import streamlit as st
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+
+# Load the model
+model = load_model("best_model.h5")
+
+# Create tokenizer function
+def predict_next_words(model, text, num_to_predict):
+    for _ in range(num_to_predict):
+        token_list = tokenizer.texts_to_sequences([text])[0]
+        token_list = pad_sequences([token_list], maxlen=5, padding='pre')
+        predicted = model.predict_classes(token_list, verbose=0)
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        text += " " + output_word
+    return text
+
+# Streamlit UI
+st.title("Shona/Ndebele Language Model")
+user_input = st.text_area("Type 5 words in Shona or Ndebele:")
+if st.button("Predict"):
+    prediction = predict_next_words(model, user_input, 1)
+    st.write(prediction)
